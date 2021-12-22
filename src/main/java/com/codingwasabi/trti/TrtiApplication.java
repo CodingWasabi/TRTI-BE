6dcc9e7bb3e6923b69fe530d81ed22f7a4d61ea5
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-22 11:10:58 +0900

    🌞[INIT]- 초기 프로젝트 생성

---
+++ src/main/java/com/codingwasabi/trti/TrtiApplication.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.trti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrtiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingwasabi/trti/TrtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingwasabi.trti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
