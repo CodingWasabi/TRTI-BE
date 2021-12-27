3dd78c544e205f0a5659f78c88e96cdd80891a85
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-28 04:28:01 +0900

    🔧[FIX]-#8-S3 Bean 미생성 오류

--- src/main/java/com/codingwasabi/trti/config/image/s3/S3ImageConfig.java
+++ src/main/java/com/codingwasabi/trti/config/image/s3/S3ImageConfig.java
@@ -1,6 +1,7 @@
 package com.codingwasabi.trti.config.image.s3;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
@@ -16,6 +17,7 @@ public class S3ImageConfig {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
+    @Bean
     public S3Client s3Client() {
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
         return S3Client.builder()
