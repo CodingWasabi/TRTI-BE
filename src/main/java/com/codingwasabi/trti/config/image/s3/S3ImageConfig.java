493dc751b95438c40cd5178899a5dbddf966c22e
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-28 02:18:41 +0900

    🔧[FIX]-#8- S3 Config 수정
    ## 개요
    변경된 라이브러리에 따라 config 수정

--- src/main/java/com/codingwasabi/trti/config/image/s3/S3ImageConfig.java
+++ src/main/java/com/codingwasabi/trti/config/image/s3/S3ImageConfig.java
@@ -1,11 +1,11 @@
 package com.codingwasabi.trti.config.image.s3;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3ImageConfig {
@@ -16,14 +16,11 @@ public class S3ImageConfig {
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+    public S3Client s3Client() {
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        return S3Client.builder()
+                .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
+                .region(Region.AP_NORTHEAST_2)
                 .build();
     }
 }
