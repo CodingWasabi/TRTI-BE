8f54215b66536168a7878fa5c866ac53cf858bf0
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-28 04:02:03 +0900

    🌞[INIT]-#10-Group API interface 구성

---
+++ src/main/java/com/codingwasabi/trti/domain/group/implementation/GroupController.java
@@ -0,0 +1,6 @@
+package com.codingwasabi.trti.domain.group.implementation;
+
+import com.codingwasabi.trti.domain.group.GroupAPI;
+
+public class GroupController implements GroupAPI {
+}
---
+++ src/main/java/com/codingwasabi/trti/domain/group/implementation/GroupServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codingwasabi.trti.domain.group.implementation;
+
+import com.codingwasabi.trti.domain.group.GroupService;
+
+public class GroupServiceImpl implements GroupService {
+}
