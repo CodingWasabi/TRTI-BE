6503f8ee8bb6fc44910e1b600927e870431a6094
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-28 03:51:47 +0900

    🌞[INIT]-#13-MemberInGroup domain 구성
    ## 개요
    member 와 group 사이의 연관관계를 풀어주고
    사이에서 발생하는 로직을 구현할 interface 를 구성

---
+++ src/main/java/com/codingwasabi/trti/domain/memberInGroup/implementation/MemberInGroupServiceImpl.java
@@ -0,0 +1,6 @@
+package com.codingwasabi.trti.domain.memberInGroup.implementation;
+
+import com.codingwasabi.trti.domain.memberInGroup.MemberInGroupService;
+
+public class MemberInGroupServiceImpl implements MemberInGroupService {
+}
