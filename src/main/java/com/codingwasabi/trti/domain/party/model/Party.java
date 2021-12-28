e1c928fe01fdcee7dfdd1756df873d03a779db76
mac_daehwan2yo <daehwan7989@naver.com>
2021-12-28 12:46:14 +0900

    📈[UPDATE]-#19- Party 생성시 captain 연관관계 추가
    파티를 생성한 파티장과 연관관계를 추가한다.

--- src/main/java/com/codingwasabi/trti/domain/party/model/Party.java
+++ src/main/java/com/codingwasabi/trti/domain/party/model/Party.java
@@ -2,15 +2,21 @@ package com.codingwasabi.trti.domain.party.model;
 
 import com.codingwasabi.trti.domain.common.Period;
 import com.codingwasabi.trti.domain.location.model.Location;
+import com.codingwasabi.trti.domain.member.model.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Party extends Period {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +32,22 @@ public class Party extends Period {
 
     private boolean isAgreed;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    private Member captain;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private Location location;
+
+    public void setCaptain(Member member) {
+        // Error code
+        if (member == null) {
+            throw new IllegalArgumentException("[EROOR] 잘못된 접근입니다. (사용자가 유효하지 않음)");
+        }
+
+        this.captain = member;
+    }
+
+    public void setLocation(Location city) {
+        this.location = city;
+    }
 }
